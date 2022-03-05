8d69bc6126b2c4d182bb13e78ca02fc66961fcab
RayFreedom88 <i.musin88@mail.ru>
2022-03-05 16:42:19 +0200

    7.1.13 Добавит презентер фильтров

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import FilmsModel from './model/films';
 import FilterModel from './model/filter';
 
 import ProfileView from './view/profile';
-import NavMenuView from './view/nav-menu';
+import NavMenuFilterView from './view/nav-menu-filter';
 
+import FilterPresenter from './presenter/filter';
 import FilmsPresenter from './presenter/films';
 
 import { render } from './utils/render';
@@ -13,14 +14,6 @@ import { render } from './utils/render';
 const FILMS_COUNT = 15;
 
 const movieList = new Array(FILMS_COUNT).fill().map(generateFilms);
-
-const filters = [
-  {
-    type: 'all',
-    name: 'All movies',
-    count: 0,
-  },
-];
 
 const filmsModel = new FilmsModel();
 filmsModel.setFilms(movieList);
@@ -30,9 +23,14 @@ const filterModel = new FilterModel();
 const siteHeader = document.querySelector('.header');
 const siteMain = document.querySelector('.main');
 
+const navMenuComponent = new NavMenuFilterView();
+
+const filmsPresenter = new FilmsPresenter(siteMain, filmsModel, filterModel);
+const filterPresenter = new FilterPresenter(navMenuComponent, filterModel, filmsModel);
+
 // рендер профиля, нафигации
 render(siteHeader, new ProfileView());
-render(siteMain, new NavMenuView(filters, 'all'));
+render(siteMain, navMenuComponent);
 
-const filmsPresenter = new FilmsPresenter(siteMain, filmsModel);
+filterPresenter.init();
 filmsPresenter.init();
---
+++ src/view/nav-menu-filter.js
@@ -0,0 +1,47 @@
+import AbstractView from './abstract';
+
+const createFilterItemTpl = (filter, currentFilterType) => {
+  const {type, name, count} = filter;
+
+  return `<a
+    href="#${type}"
+    class="main-navigation__item ${type === currentFilterType ? 'main-navigation__item--active' : ''}"
+    title="${type}">
+      ${name}
+      ${type !== 'all' ? `<span class="main-navigation__item-count">${count}</span>` : ''}
+  </a>`;
+};
+
+const createNavMenuFilterTpl = (filterItems, currentFilterType) => {
+  const filterItemsTpl = filterItems
+    .map((filter) => createFilterItemTpl(filter, currentFilterType))
+    .join('');
+
+  return `<div class="main-navigation__items">
+    ${filterItemsTpl}
+    </div>`;
+};
+
+export default class NavMenuFilter extends AbstractView {
+  constructor(filters, currentFilterType) {
+    super();
+    this._filters = filters;
+    this._currentFilter = currentFilterType;
+
+    this._filterClickHandler = this._filterClickHandler.bind(this);
+  }
+
+  getTemplate() {
+    return createNavMenuFilterTpl(this._filters, this._currentFilter);
+  }
+
+  _filterClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.filterClick(evt.target.title);
+  }
+
+  setFilterClickHandler(callback) {
+    this._callback.filterClick = callback;
+    this.getElement().addEventListener('click', this._filterClickHandler);
+  }
+}
